ed2e0dd4bc2569b9b3f5fd3339ed3a794163897a
Wayne Lai <waynelai01011231@gmail.com>
2021-02-25 20:35:16 +0800

    Test list download method

--- Windows/download_sysinternals.bat
+++ Windows/download_sysinternals.bat
@@ -1,2 +1,15 @@
-certutil -urlcache -split -f "http://live.sysinternals.com/tcpview.exe" tcpview.exe
-echo "downloaded tcpview.exe"
+@echo off
+md sysinternals
+cd sysinternals
+
+set sys_tools[0] = "accesschk.exe"
+set sys_tools[1] = "AccessEnum.exe"
+set sys_tools[2] = "ADExplorer.exe"
+set sys_tools[3] = "ADInsight.exe"
+
+(for %%t in (%sys_tools%) do (
+  IF NOT EXIST %%t (
+    certutil -urlcache -split -f "http://live.sysinternals.com/%%t" %%t
+    echo "downloaded %%t"
+  )
+))
