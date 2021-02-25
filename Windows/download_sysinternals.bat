e651ea3ee2af4c5d483d155f8a66861ecb60c06a
Wayne Lai <waynelai01011231@gmail.com>
2021-02-25 22:37:53 +0800

    Organise code into list and for loop structure

--- Windows/download_sysinternals.bat
+++ Windows/download_sysinternals.bat
@@ -1,15 +1,25 @@
 @echo off
-md sysinternals
-cd sysinternals
+setlocal EnableDelayedExpansion
+IF NOT EXIST sysinternals md sysinternals
 
 set sys_tools[0] = "accesschk.exe"
 set sys_tools[1] = "AccessEnum.exe"
 set sys_tools[2] = "ADExplorer.exe"
 set sys_tools[3] = "ADInsight.exe"
+set sys_tools[4] = "adrestore.exe"
+set sys_tools[5] = "Autologon.exe"
 
-(for %%t in (%sys_tools%) do (
-  IF NOT EXIST %%t (
-    certutil -urlcache -split -f "http://live.sysinternals.com/%%t" %%t
-    echo "downloaded %%t"
+set "x=0"
+
+:SymLoop
+if defined sys_tools[%x%] (
+  SET output_path=%~dp0sysinternals\!systools[%x%]!
+  IF NOT EXIST "%output_path%" (
+    SET sys_url="http://live.sysinternals.com/!systools[%x%]!"
+    bitsadmin /transfer "%output_path%" /download /priority normal %sys_url% %output_path%
+  ) ELSE (
+    echo "%output_path% exists"
   )
-))
+  set /a "x+=1"
+  GOTO :SymLoop
+)
